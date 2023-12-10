639a227daa18157b4ed08f33ca7231fa064021b2
Flown92 <117080609+Flown92@users.noreply.github.com>
2023-12-10 12:57:12 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import torch
-import cv2
-import pandas
 import os
 from os import listdir
 from os.path import isfile, join
+from ultralytics import YOLO
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights.pt', force_reload=True)
+model = YOLO('weights.pt')
+folder_path = "result"
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
 folder_path = "result"
 
-# Если папка не существует, создаем ее
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
@@ -23,15 +25,16 @@ for file in files:
     filepath = mypath + "\\" + file
     img = Image.open(filepath)
     results = model(img)
-    labels = results.pandas().xyxy
+    labels_2 = results[0].boxes.xyxy.tolist()
     with open(final_path + "\\" + file[0:len(file) - 3] + "txt", 'w') as resfile:
-        print(labels)
-        if len(labels) == 0:
+        if len(labels_2) == 0:
             continue
-        for i in range(labels[0].shape[0]):
-
-            if len(labels[0]) > 0:
-                lx = labels[0]["xmax"][i] - labels[0]["xmin"][i]
-                ly = labels[0]["ymax"][i] - labels[0]["ymin"][i]
-                mid = (labels[0]["xmin"][i] + lx / 2, labels[0]["ymin"][i] + ly / 2)
-                resfile.write(", ".join(["Class_" + str(labels[0]["class"][i]), str(mid[0]), str(mid[1]), str(lx), str(ly)]) + '\n')
+        for lp in labels_2:
+            try:
+                xmin_pred, ymin_pred, xmax_pred, ymax_pred = lp[0], lp[1], lp[2], lp[3]
+                lx = xmax_pred - xmin_pred
+                ly = ymax_pred - ymin_pred
+                mid = (xmin_pred + lx / 2, ymin_pred + ly / 2)
+                resfile.write(", ".join([str(mid[0]), str(mid[1]), str(lx), str(ly)]) + "\n")
+            except Exception:
+                continue
